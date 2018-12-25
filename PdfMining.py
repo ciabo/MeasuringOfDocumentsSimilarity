96ca3bc49ce4f2301380a842c172d815f8a8bed4
Luca <lucaciabo@hotmail.it>
2018-12-25 11:36:21 +0100

    Fixed compatibilty problems with python 3 and windows

--- PdfMining.py
+++ PdfMining.py
@@ -3,7 +3,7 @@ from pdfminer.pdfpage import PDFPage
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
 from nltk.corpus import stopwords
-from io import StringIO  # for python 3.x use: from io import StringIO
+from cStringIO import StringIO  # for python 3.x use: from io import StringIO
 import os
 import re
 
@@ -37,22 +37,23 @@ filename = "./Document/" + pdfname + ".pdf"
 # create a txt to tokenize
 txt = pdf_to_text(filename)
 txtname = "./txt/" + pdfname + ".txt"
-txtFile = open(txtname, "w")
+txtFile = open(txtname, "w",encoding='utf-8')
 txtFile.write(txt)
 text = []
 txtFile.close()
 
 tokenName = "./txt/" + pdfname + "2.txt"  # txt that contains token
-tokenFile = open(tokenName, "w+")
+tokenFile = open(tokenName, "w+",encoding='utf-8')
 
 punctuations = ['(', ')', ';', ':', '[', ']', ',', '']
-stop_words = [word.encode('utf-8') for word in stopwords.words('english')]  # transform utf-8 str to byte str
-with open(txtname, "r") as f:
+stop_words = [word.encode('utf-8') for word in stopwords.words('english')]
+
+with open(txtname, "r",encoding='utf-8') as f:
     for line in f:
         for word in re.split(r',|\.|;|:|\s|\(|\)|\[|\]|\"|<|>|-|=', line):
             if word not in punctuations and word not in stop_words:
                 text.append(word)
                 tokenFile.write(word + "\n")
-    print (text)
+    print(text)
 tokenFile.close()
 os.remove("./txt/" + pdfname + ".txt")  # erase the txt used to tokenize
