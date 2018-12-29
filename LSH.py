ba4b9d9b0378268cfa3e21ed0d887fbb142b3e89
Luca <lucaciabo@hotmail.it>
2018-12-29 15:52:24 +0100

    fixed hash table dimension in LSH

--- LSH.py
+++ LSH.py
@@ -1,9 +1,10 @@
 import math
 
-#--------BUG: se ci sono 7righe nei minhash e divido per 3 mi da 2 bande da 3 e schianta perchè la terza banda è da 1
+#--------FEATURE: se ci sono 7righe nei minhash e divido per 3 mi da 2 bande da 3 e schianta perchè la terza banda è da 1
 def LSH(minHashes, numberOfBands, numberOfDocuments):
     bandDim = len(minHashes) // numberOfBands  # For python 2 change // with /
-    newHashDim = int(math.pow(10,bandDim)) #10^bandDim
+    #newHashDim = int(math.pow(10,bandDim)) #10^bandDim
+    newHashDim = numberOfDocuments*numberOfBands*100 #total number of possible values * 100
     documentCandidates = []
     for i in range(0, numberOfBands):
         bandedMinHashes = []
@@ -29,7 +30,9 @@ def LSH(minHashes, numberOfBands, numberOfDocuments):
 def listHash(list,m):
     encode=""
     for i in range(0,len(list)):
-        encode+=str(list[i])
+        intList=int(list[i])
+        strList=str(intList)
+        encode=encode + strList
     val=int(encode)
     newHash=val%m
     return newHash
--- main.py
+++ main.py
@@ -8,27 +8,28 @@ import os
 
 
 def main():
-    # pdfdir = "Document"  # pdf directory
-    # rename_pdf(pdfdir)
+    #pdfdir = "Document"  # pdf directory
+    #rename_pdf(pdfdir)
 
-    m = 10000009  # dieci milioni e 9
+    m = 1000000  # dieci milioni e 9
     matrix = [[] for i in range(0, m)]
     minHashes = [[] for i in range(0, 100)]
 
     txtdir = "txt"  # select txt directory
     path = './Document/'
-    num_files = len([f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))])  # minHash()
+    numFiles = len([f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))])  # minHash()
 
-    # for i in range(0, num_files):
-    #    tokenizePdf("Doc" + str(i), txtdir)
-    #    generateShingles("Doc" + str(i), txtdir)
-    # print("Mining ended")
-    # print("Shingles generated")
-    for i in range(0, num_files):
+    for i in range(0, numFiles):
+        tokenizePdf("Doc" + str(i), txtdir)
+        generateShingles("Doc" + str(i), txtdir)
+    print("Mining ended")
+    print("Shingles generated")
+    for i in range(0, numFiles):
         generateHash("Doc" + str(i), txtdir, m, matrix, i)
     print("Matrix of hashes generated")
-    minHashes = minHash(matrix, num_files, m)
-    # LSH()
+    minHashes = minHash(matrix, numFiles, m,100)
+    results=LSH(minHashes,5,numFiles)
+    print(results)
 
 
 if __name__ == '__main__':
