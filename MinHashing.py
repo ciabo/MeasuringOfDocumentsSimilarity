1f577c0d751c10489f7ceda1039d204ec2cbc481
ettore <ettorecelozzi@gmail.com>
2018-12-29 10:59:37 +0100

    minHash bug fix

--- MinHashing.py
+++ MinHashing.py
@@ -1,7 +1,7 @@
 from random import randint
 import numpy as np
 
-primeNumber = 7919  # this must be greater than the number of shingles
+primeNumber = 10000013  # this must be greater than the number of shingles(dieci milioni e 13)
 
 
 # matrix is a vector of dim equals to the number of shingles and
@@ -12,12 +12,12 @@ def minHash(matrix, numberOfDocuments, shingleNumber, permutationNumber=100):
     minHashes = []
     for i in range(0, permutationNumber):
         hashedIndex = []  # hashedIndex is a vector that contains a permutation: hashedIndex[0]=3 means that the row 0 will be the row 3
-        minHash = np.full((1, numberOfDocuments), np.inf)  # vector with dimension equal to the number of documents
+        minHash = np.full(numberOfDocuments, np.inf)  # vector with dimension equal to the number of documents
         a = randint(0, 100)
         b = randint(0, 100)
         for j in range(0, shingleNumber):
             hashedIndex.append(hash(j, primeNumber, shingleNumber, a, b))
-            ones = matrix[j]
+            ones = matrix[j]  # signature
             for k in ones:
                 if hashedIndex[j] < minHash[k]:
                     minHash[k] = hashedIndex[j]  # minHash contain the value of the row corresponding to the one
@@ -26,6 +26,6 @@ def minHash(matrix, numberOfDocuments, shingleNumber, permutationNumber=100):
     return minHashes
 
 
-def hash(val, p, n,a,b):
+def hash(val, p, n, a, b):
     res = ((a * val + b) % p) % n
     return res
--- main.py
+++ main.py
@@ -13,20 +13,21 @@ def main():
 
     m = 10000009  # dieci milioni e 9
     matrix = [[] for i in range(0, m)]
+    minHashes = [[] for i in range(0, 100)]
 
     txtdir = "txt"  # select txt directory
     path = './Document/'
     num_files = len([f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))])  # minHash()
 
-    for i in range(0, num_files):
-        tokenizePdf("Doc" + str(i), txtdir)
-        generateShingles("Doc" + str(i), txtdir)
-    print("Mining ended")
-    print("Shingles generated")
+    # for i in range(0, num_files):
+    #    tokenizePdf("Doc" + str(i), txtdir)
+    #    generateShingles("Doc" + str(i), txtdir)
+    # print("Mining ended")
+    # print("Shingles generated")
     for i in range(0, num_files):
         generateHash("Doc" + str(i), txtdir, m, matrix, i)
     print("Matrix of hashes generated")
-    # minHashing()
+    minHashes = minHash(matrix, num_files, m)
     # LSH()
 
 
