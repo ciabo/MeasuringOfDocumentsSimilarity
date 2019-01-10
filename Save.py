b03dbd909aec935369971b4d115b24a166232ac4
ettore <ettorecelozzi@gmail.com>
2019-01-10 14:28:08 +0100

    Confront one document with db

--- Save.py
+++ Save.py
@@ -1,15 +1,14 @@
 def SaveMinHash(minHash):
     txtname = "./minHashes/minHash.txt"
-
     with open(txtname, "w+", encoding='utf-8') as f:
         for list in minHash:
             for element in list:
                 f.write(str(int(element)) + ",")
             f.write("\n")
 
+
 def Saveab(ablist):
     txtname = "./minHashes/ab.txt"
-
     with open(txtname, "w+", encoding='utf-8') as f:
         for element in ablist:
-                f.write(str(element[0]) + ","+str(element[1]) +"\n")
+            f.write(str(element[0]) + "," + str(element[1]) + "\n")
--- main.py
+++ main.py
@@ -5,18 +5,20 @@ from Shingling import generateShingles
 from MinHashing import minHash
 from StringHashing import generateHash
 from SparseMatrix import SparseMatrix
+from Save import SaveMinHash
+from OneDocSimilar import docsimilar
 import os
 
 
 def main():
-    pdfdir = "data"  # pdf directory
+    pdfdir = "Document"  # pdf directory
     rename_pdf(pdfdir)
 
     numberOfPermutations = 100  # number of permutation in the minHashing phase
     numberOfBands = 25  # number of bands in LSH phase
     m = 1000003
     matrix = SparseMatrix()
-    txtdir = "txtdata"  # select txt directory
+    txtdir = "txt"  # select txt directory
     path = './' + pdfdir + '/'
     numFiles = len([f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))])  # minHash()
     for i in range(0, numFiles):
@@ -28,8 +30,13 @@ def main():
         generateHash("Doc" + str(i), txtdir, m, matrix, i)
     print("Matrix of hashes generated")
     minHashes = minHash(matrix, numFiles, m, numberOfPermutations)
+    SaveMinHash(minHashes)
     results = LSH(minHashes, numberOfBands, numFiles)
     print(results)
+
+    matr = SparseMatrix()
+    docsimilar("./prova.pdf", "./OneDocSimilar/", matr, m, numFiles, numberOfPermutations, numberOfBands,
+               "./minHashes/minHash.txt", "./minHashes/ab.txt")
 
 
 if __name__ == '__main__':
