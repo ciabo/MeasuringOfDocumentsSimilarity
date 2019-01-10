635e9a891ddfe0c8d8a2fff041ef153375d27980
Luca <lucaciabo@hotmail.it>
2019-01-10 19:48:21 +0100

    Bug fix

--- main.py
+++ main.py
@@ -11,36 +11,6 @@ from ParseData import parsedata
 import time
 import os
 
-
-def main():
-    numberOfPermutations = 100  # number of permutation in the minHashing phase
-    numberOfBands = 25  # number of bands in LSH phase
-    m = 1000003
-
-    t0 = time.time()
-    matrix = SparseMatrix()
-    numFiles = parsedata("data2017", "txtdata", matrix, 0, m, True)
-    n=numFiles
-    numFiles += parsedata("data2018", "txtdata", matrix, numFiles, m, True)
-    print(numFiles)
-    minHashes = minHash(matrix, numFiles, m, numberOfPermutations)
-    SaveMinHash(minHashes)
-    results = LSH(minHashes, numberOfBands, numFiles)
-    results = cleanResults(results,n)
-    print(results)
-    print(time.time() - t0)
-    print(" ")
-
-    t1 = time.time()
-    matrice = SparseMatrix()
-    docsimilar("./Doc4", "./OneDocSimilar/", matrice, m, numFiles, numberOfPermutations, numberOfBands,
-               "./minHashes/minHash.txt", "./minHashes/ab.txt")
-    print(time.time() - t1)
-
-
-if __name__ == '__main__':
-    main()
-
 def cleanResults(results,n):
     toBeRemoved = []
     for i in range(0, len(results)):
@@ -57,3 +27,35 @@ def cleanResults(results,n):
     for i in toBeRemoved:
         results.pop(i)
     return results
+
+
+def main():
+    numberOfPermutations = 100  # number of permutation in the minHashing phase
+    numberOfBands = 20  # number of bands in LSH phase
+    m = 1000003
+
+    t0 = time.time()
+    matrix = SparseMatrix()
+    numFiles = parsedata("data2017", "txtdata", matrix, 0, m, False)
+    n=numFiles
+    numFiles += parsedata("data2018", "txtdata", matrix, numFiles, m, False)
+    print(numFiles)
+    minHashes = minHash(matrix, numFiles, m, numberOfPermutations)
+    SaveMinHash(minHashes)
+    results = LSH(minHashes, numberOfBands, numFiles)
+    print(results)
+    results = cleanResults(results,n)
+    print(results)
+    print(time.time() - t0)
+    print(" ")
+
+    t1 = time.time()
+    matrice = SparseMatrix()
+    docsimilar("./Doc4", "./OneDocSimilar/", matrice, m, numFiles, numberOfPermutations, numberOfBands,
+               "./minHashes/minHash.txt", "./minHashes/ab.txt")
+    print(time.time() - t1)
+
+
+if __name__ == '__main__':
+    main()
+
